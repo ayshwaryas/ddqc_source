e1ffde6ba31668d3356440c755114410ff2e9789
Mikhail Alperovich <misha.alperovich1@gmail.com>
2022-04-29 20:22:04 -0400

    path fix

--- ddqc_pipeline/submit_all.py
+++ ddqc_pipeline/submit_all.py
@@ -26,7 +26,7 @@ use .python-3.8.3
 use R-3.5
 
 source /home/unix/malperov/pegasusenv/bin/activate
-cd /home/unix/malperov/Primes2019/ddqc_pipeline/
+cd /home/unix/malperov/Primes-2019/ddqc_pipeline/
 python submission/run_tissue.py {project} {tissue} {method} {run_analysis}"""
 
 
